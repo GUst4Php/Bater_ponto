90d5ca2373d1f952e6ae4c8a0903bdb9ace08d2f
Paulo <eu.paulogustavoandrade@gmail.com>
2025-11-30 21:45:37 -0300

    MExendo nas funcionalidades de db

--- backend/class_obj.php
+++ backend/class_obj.php
@@ -1,73 +1,186 @@
 <?php
-    //A classe funcionário é a classe principal, interligada ao banco de dados, 
-    //Ela cadastra, faz consulta, verifica existência, edita dados
 
-    class Funcionario {
-        // atributos
-        protected string $nome;
-        protected string $email;
-        protected string $telefone;
-        protected float $salario;
-        protected int $horas;
-        protected int $nivel;
-        protected bool $ativo;
+require_once './funcoes_db.php';
+//A classe funcionário é a classe principal, interligada ao banco de dados, 
+//Ela cadastra, faz consulta, verifica existência, edita dados
 
-        public function __construct(string $nome, string $email, string $telefone, float $salario, int $horas, int $nivel, bool $ativo){
-            $this->__setNome($nome);
-            $this->__setEmail($email);
-            $this->__setTelefone($telefone);
-            $this->__setSalario($salario);
-            $this->__setHoras($horas);
-            $this->__setNivel($nivel);
-            $this->__setAtivo($ativo);
-        }
-        //Metodos de inserçao da função
-        private function __setNome(string $nome){
-            $this->nome = $nome;
-        }
-        private function __setEmail(string $email){
-            $this->email = $email;
-        }
-        private function __setTelefone(string $telefone){
-            $this->telefone = $telefone;
-        }
-        private function __setSalario(float $salario) {
-            $this->salario = $salario;
-        }
-        private function __SetHoras(int $horas){
-            $this->horas = $horas;
-        }
-        private function __setNivel(int $nivel){
-            $this->nivel = $nivel;
-        }
-        private function __setAtivo(bool $ativo){
-            $this->ativo = $ativo;
-        }
+class Funcionario
+{
+    // atributos
+    protected string $nome;
+    protected string $email;
+    protected string $telefone;
+    protected float $salario;
+    protected int $horas;
+    protected int $nivel;
+    protected bool $ativo;
 
-        //metodos de captura da função
-        public function __getNome(){
-            return $this->nome;
-        } 
-        public function __getEmail(){
-            return $this->email;
-        }
-        public function __getTelefone(){
-            return $this->telefone;
-        }
-        public function __getSalario(){
-            return $this->salario;
-        }
-        public function __getHoras(){
-            return $this->horas;
-        }
-        public function __getNivel(){
-            return $this->nivel;
-        }
-        public function __getAtvivo(){
-            return $this->ativo;
-        }
-        };
-
-    class FuncionarioRepository{
-        
+    public function __construct(string $nome, string $email, string $telefone, float $salario, int $horas, int $nivel, bool $ativo)
+    {
+        $this->__setNome($nome);
+        $this->__setEmail($email);
+        $this->__setTelefone($telefone);
+        $this->__setSalario($salario);
+        $this->__setHoras($horas);
+        $this->__setNivel($nivel);
+        $this->__setAtivo($ativo);
     }
+    //Metodos de inserçao da função
+    private function __setNome(string $nome)
+    {
+        $this->nome = $nome;
+    }
+    private function __setEmail(string $email)
+    {
+        $this->email = $email;
+    }
+    private function __setTelefone(string $telefone)
+    {
+        $this->telefone = $telefone;
+    }
+    private function __setSalario(float $salario)
+    {
+        $this->salario = $salario;
+    }
+    private function __SetHoras(int $horas)
+    {
+        $this->horas = $horas;
+    }
+    private function __setNivel(int $nivel)
+    {
+        $this->nivel = $nivel;
+    }
+    private function __setAtivo(bool $ativo)
+    {
+        $this->ativo = $ativo;
+    }
+
+    //metodos de captura da função
+    public function __getNome()
+    {
+        return $this->nome;
+    }
+    public function __getEmail()
+    {
+        return $this->email;
+    }
+    public function __getTelefone()
+    {
+        return $this->telefone;
+    }
+    public function __getSalario()
+    {
+        return $this->salario;
+    }
+    public function __getHoras()
+    {
+        return $this->horas;
+    }
+    public function __getNivel()
+    {
+        return $this->nivel;
+    }
+    public function __getAtvivo()
+    {
+        return $this->ativo;
+    }
+};
+
+
+//classe que faz a interligação com o banco de dados
+//armazena funções de CRUD, Create, Read, Update, Delete
+//recebe a conexão com o banco de dados via PDO
+//e um objeto funcionário para manipular os dados
+class FuncionarioRepository
+{
+    public function __construct(Funcionario $funcionario, protected PDO $conexao)
+    {
+        $conexao = $this->conexao;
+        $funcionario = $funcionario;
+    }
+
+    //função de cadastro de funcionário
+    //retorna true se cadastrado com sucesso, false se falhar
+    public function cadastrar(Funcionario $funcionario): bool
+    {
+        $sql = "INSERT INTO funcionarios (nome, email, telefone, salario, horas, nivel, ativo) VALUES (:nome, :email, :telefone, :salario, :horas, :nivel, :ativo)";
+        if ($this->conexao) {
+            $stmt = $this->conexao->prepare($sql);
+            $stmt->execute([
+                ':nome' => $funcionario->__getNome(),
+                ':email' => $funcionario->__getEmail(),
+                ':telefone' => $funcionario->__getTelefone(),
+                ':salario' => $funcionario->__getSalario(),
+                ':horas' => $funcionario->__getHoras(),
+                ':nivel' => $funcionario->__getNivel(),
+                ':ativo' => $funcionario->__getAtvivo()
+            ]);
+            return True;
+        } else {
+            return False;
+        }
+    }
+
+    //função de inativar funcionário
+    //retorna true se inativado com sucesso, false se falhar
+    public function InativarFuncionario(Funcionario $funcionario): bool
+    {
+        $nome = $funcionario->__getNome();
+        $query = "update funcionarios set ativo = 0 qhere nome = :nome";
+        if ($this->conexao) {
+            $stmt = $this->conexao->prepare($query);
+            $stmt->execute([':nome' => $nome]);
+            return True;
+        } else {
+            return False;
+        }
+    }
+
+    //função de consulta de funcionário
+    //retorna um objeto funcionário com os dados preenchidos
+
+    public function __getDadosFuncionario(Funcionario $funcionario): Funcionario
+    {
+
+        $nome = $funcionario->__getNome();
+        $query = "select * from funcionarios where nome = :nome";
+        if ($this->conexao) {
+            $stmt = $this->conexao->prepare($query);
+            $stmt->execute([':nome' => $nome]);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            $funcionario = new Funcionario(
+                $result['nome'],
+                $result['email'],
+                $result['telefone'],
+                $result['salario'],
+                $result['horas'],
+                $result['nivel'],
+                $result['ativo']
+            );
+            return $funcionario;
+        } else {
+            throw new Exception("Conexão com o banco de dados falhou.");
+        }
+    }
+    //função de edição de funcionário
+    //retorna true se editado com sucesso, false se falhar
+    public function editarFunc(Funcionario $funcionario): bool
+    {
+        $sql = "UPDATE funcionarios SET nome = :nome, email = :email, telefone = :telefone, salario = :salario, horas = :horas, nivel = :nivel, ativo = :ativo WHERE nome = :nome";
+        if ($this->conexao) {
+            $stmt = $this->conexao->prepare($sql);
+            $stmt->execute([
+                ':nome' => $funcionario->__getNome(),
+                ':email' => $funcionario->__getEmail(),
+                ':telefone' => $funcionario->__getTelefone(),
+                ':salario' => $funcionario->__getSalario(),
+                ':horas' => $funcionario->__getHoras(),
+                ':nivel' => $funcionario->__getNivel(),
+                ':ativo' => $funcionario->__getAtvivo()
+            ]);
+            return True;
+        } else {
+            return False;
+        }
+    }
+}
--- backend/funcoes_db.php
+++ backend/funcoes_db.php
@@ -1,23 +1,37 @@
 <?php
+require './class_obj.php';
+//Esta função faz conexão direta e apenas com o banco de dados de funcionários
+//ela retorna a conexão, que vai ser utilizada pela classe FuncionarioRepository
+function DB_funcionarios()
+{
+    $host = '127.0.0.1';
+    $db = 'nexu';
+    $user = 'admin';
+    $password = '0000';
 
-    //Esta função faz conexão direta e apenas com o banco de dados de funcionários
-    //ela retorna a conexão, que vai ser utilizada pela classe FuncionarioRepository
-    function DB_funcionarios(){
-        $host = '127.0.0.1';
-        $db = 'nexu';
-        $user = 'admin';
-        $password = '0000';
+    try {
+        $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+        $pdo = new PDO($dsn, $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (Exception $e) {
+        echo "Erro na conexão com banco de dados Nexu!!, Favor, fazer chamado!" . $e->getMessage();
 
-        try {
-            $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
-            $pdo = new PDO($dsn, $user, $password);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $pdo;
-
-        }   catch (Exception $e){
-            echo "Erro na conexão com banco de dados Nexu!!, Favor, fazer chamado!" . $e->getMessage();
-            
-            return Null;
-
-        }
+        return Null;
     }
+}
+
+function salarioHora(float $salario, int $horas): float
+{
+    $salariohora = $salario / $horas;
+    return $salariohora;
+}
+
+function salarioFuncionario(FuncionarioRepository $funcionarioRepo): float
+{
+    $funcionario = $funcionarioRepo->__getDadosFuncionario($funcionario);
+    $salarioHora = salarioHora($funcionario->__getSalario(), $funcionario->__getHoras());
+    $salario = $salarioHora * $funcionario->__getHoras();
+
+    return $salario;
+}
